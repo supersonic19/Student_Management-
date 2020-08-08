7d16f5dddfd03ab79677de4ef136f45efe0b2ad0
Swarad Gat <swarad2000@gmail.com>
2020-08-08 17:49:13 +0530

    commit on 'master'

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,9 @@ from main import views
 urlpatterns = [
     path('',views.Index.as_view()),
     path('college/<int:pk>',views.CollegeDetail.as_view(),name = 'college'),
-    path('college/' , views.CollegeList.as_view())
+    path('college/' , views.CollegeList.as_view()),
+    path('college_create/' , views.CollegeCreate.as_view()),
+    path('update_college/<int:pk>' , views.UpdateCollege.as_view()),
+    path('student_create/' , views.StudentCreate.as_view()),
+    path('student_list/' , views.StudentList.as_view())
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import (
     DetailView,
-    ListView
+    ListView,
+    CreateView,
+    UpdateView
 )
 
 from main import models
@@ -29,10 +31,38 @@ Django provides us with 5 generic views:
 class CollegeDetail(DetailView):
     model = models.College
     template_name = 'college_detail.html'
+    fields = '__all__'
 
 class CollegeList(ListView):
     model = models.College
     template_name = 'college_list.html'
-    context_object_name = 'colleges'    
+    fields = '__all__'
+    context_object_name = 'colleges'  
+
+class CollegeCreate(CreateView):
+    model = models.College
+    template_name = 'college_create.html'
+    fields = "__all__"
+    success_url = '/college'
+
+class StudentCreate(CreateView):
+    model = models.Student
+    template_name = 'student_create.html'
+    fields = '__all__'
+    success_url = '/college'
+
+class UpdateCollege(UpdateView):
+    model = models.College
+    template_name = 'college_create.html'
+    fields = '__all__'
+    success_url = '/college'
+
+class StudentList(ListView):
+    model = models.Student
+    model2 = models.College
+    template_name = 'student_list.html'
+    fields = '__all__'
+    context_object_name = 'students'
+    
 
 
